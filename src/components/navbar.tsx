c92090c3b77266abadf9dce6d40d2b042132edc3
Angela Xu <angela.xu.dev@gmail.com>
2025-05-24 22:26:41 +0900

    navbar basic

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,18 +3,35 @@ import Image from 'next/image';
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center p-4 border-b-4 border-indigo-800">
-      <div className="flex items-center gap-4">
-        <Image src="/logo.png" alt="logo" width={32} height={32} />
-        <h1 className="">About</h1>
-        <h1 className="">Sponsors</h1>
-        <h1 className="">Keynote</h1>
-        <h1 className="">FAQ</h1>
-        <h1 className="">Team</h1>
-        <h1 className="">Contact Us</h1>
+    <div className="flex justify-between items-center border-b-2 border-navSecondary bg-navPrimary px-24 py-3">
+      <div className="flex items-center gap-8 ">
+        <Image
+          src="/Technova2024Logo512.png"
+          alt="logo"
+          width={40}
+          height={40}
+        />
+        <a href="/" className="">
+          About
+        </a>
+        <a href="/" className="">
+          Sponsors
+        </a>
+        <a href="/" className="">
+          Keynote
+        </a>
+        <a href="/" className="">
+          FAQ
+        </a>
+        <a href="/" className="">
+          Team
+        </a>
+        <a href="/" className="">
+          Contact Us
+        </a>
       </div>
-      <button className="flex items-center gap-4 bg-indigo-800 text-white p-4">
-        <h1 className="">Apply</h1>
+      <button className="flex items-center bg-navSecondary text-white px-4 py-2 rounded-md">
+        <h1 className="">Your Application</h1>
       </button>
     </div>
   );
