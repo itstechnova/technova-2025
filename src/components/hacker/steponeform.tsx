0cc480d0ebd57bf87ddcc5d3934c1bbcee97042c
Angela Xu <angela.xu.dev@gmail.com>
2025-05-29 14:03:19 -0700

    questions

--- src/components/hacker/steponeform.tsx
+++ src/components/hacker/steponeform.tsx
@@ -10,65 +10,67 @@ interface HackerLandingFormProps {
 function HackerStepOneForm() {
   return (
     <div className="flex flex-col h-full bg-navPrimary relative">
-      <div className="absolute top-0 left-0 w-full h-1/4 pointer-events-none z-0 bg-gradient-to-b from-backgroundSecondary to-navPrimary">
-        <div className="p-24">
-          <div className="flex gap-2 items-center pb-8">
-            <h1 className="text-5xl font-semibold text-textSecondary z-1">
-              Let's get to know you better!
-            </h1>
-            <Image
-              src="/themed_assets/Sunflower.png"
-              alt="grass"
-              width={40}
-              height={40}
-            />
-          </div>
+      <div className="p-24 bg-gradient-to-b from-backgroundSecondary to-navPrimary">
+        <div className="flex gap-2 items-center pb-10">
+          <h1 className="text-5xl font-semibold text-textSecondary z-1">
+            Let's get to know you better!
+          </h1>
+          <Image
+            src="/themed_assets/Sunflower.png"
+            alt="grass"
+            width={40}
+            height={40}
+          />
+        </div>
 
-          <form className="form">
-            <div className="flex flex-col gap-24 text-textPrimary">
-              <div className="flex gap-auto w-full">
-                <div className="flex flex-col gap-5">
-                  <label className="text-base font-bold">Email</label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-buttonSecondary shadow-xs shadow-navSecondary"
-                    placeholder="ex. janesmith@gmail.com"
-                  />
-                </div>
-                <div className="flex flex-col gap-5">
-                  <label className="text-base font-bold">Email</label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-buttonSecondary shadow-xs shadow-navSecondary"
-                    placeholder="ex. janesmith@gmail.com"
-                  />
-                </div>
-              </div>
+        <form className="form">
+          <div className="flex flex-col gap-24 text-textPrimary">
+            <div className="grid grid-cols-2 gap-10 w-full">
               <div className="flex flex-col gap-5">
                 <label className="text-base font-bold">
-                  How old will you be as of September 27, 2025?
+                  What's your first name?
                 </label>
                 <input
                   type="text"
-                  name="age2025"
-                  id="age2025"
+                  name="email"
+                  id="email"
                   className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-buttonSecondary shadow-xs shadow-navSecondary"
-                  placeholder="ex. 21"
+                  placeholder="ex. Jane"
+                />
+              </div>
+              <div className="flex flex-col gap-5">
+                <label className="text-base font-bold">
+                  What's your last name?
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-buttonSecondary shadow-xs shadow-navSecondary"
+                  placeholder="ex. Smith"
                 />
               </div>
             </div>
-            <button
-              type="submit"
-              className="bg-buttonPrimary px-8 py-2 mt-24 text-white text-2xl rounded-xl w-auto shadow-xs shadow-navSecondary"
-            >
-              →
-            </button>
-          </form>
-        </div>
+            <div className="flex flex-col gap-5">
+              <label className="text-base font-bold">
+                What are your pronouns?
+              </label>
+              <input
+                type="text"
+                name="age2025"
+                id="age2025"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-buttonSecondary shadow-xs shadow-navSecondary"
+                placeholder="ex. she/her/hers (all lowercase)"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-buttonPrimary px-8 py-2 mt-24 text-white text-2xl rounded-xl w-auto shadow-xs shadow-navSecondary"
+          >
+            →
+          </button>
+        </form>
       </div>
     </div>
   );
