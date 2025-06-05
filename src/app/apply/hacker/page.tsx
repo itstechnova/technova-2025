6ef595dd0f0f3e30cf351a227c4585d41b27efe3
Angela Xu <angela.xu.dev@gmail.com>
2025-06-05 14:47:54 -0700

    integrate hacker landing submissions

--- src/app/apply/hacker/page.tsx
+++ src/app/apply/hacker/page.tsx
@@ -2,16 +2,50 @@
 
 import React, { useState } from 'react';
 import HackerLandingForm from '@/components/hacker/landingform';
+import supabase from '@/config/supabaseClient';
+import { useRouter } from 'next/navigation';
 
 function HackerLanding() {
+  const [formError, setFormError] = useState<string | null>(null);
   const [landingData, setLandingData] = useState({
     email: '',
     age2025: '',
   });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const requiredFields = ['email', 'age2025'];
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(JSON.stringify(landingData));
+    if (
+      requiredFields.some(
+        (field) => !landingData[field as keyof typeof landingData]
+      )
+    ) {
+      setFormError('Please fill in all required fields');
+      return;
+    } else {
+      setFormError(null);
+    }
+
+    const { data, error } = await supabase
+      .from('hacker_landing')
+      .insert([
+        {
+          email: landingData.email,
+          age2025: landingData.age2025,
+        },
+      ])
+      .select();
+    if (error) {
+      setFormError('Error submitting form');
+      console.log('error', error);
+    }
+    if (data) {
+      setFormError(null);
+      router.push('/apply/hacker/step-one');
+      console.log(JSON.stringify(landingData));
+    }
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,6 +65,7 @@ function HackerLanding() {
         data={landingData}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
+        formError={formError}
       />
     </div>
   );
