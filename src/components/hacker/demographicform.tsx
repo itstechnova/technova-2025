725de66654a1ce5269795b9bd056ab5bd52f07ae
Kim Guo <kbrqin@gmail.com>
2025-06-08 18:47:57 -0400

    [hacker-app] created demographic form
    created demographic form (1.4) and updated checkoff component to allow "other" input

---
+++ src/components/hacker/demographicform.tsx
@@ -0,0 +1,203 @@
+import React from "react";
+import Image from "next/image";
+import SubmitButton from "../submitButton";
+import CheckOff from "../checkOff";
+import MultiCheckbox from "./MultiCheckbox";
+
+interface HackerDemographicFormProps {
+  data: any;
+  setData: React.Dispatch<React.SetStateAction<any>>;
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const ethnicityOptions = [
+  "Hispanic/Latinx",
+  "Black/African Descent",
+  "Indigenous",
+  "Middle Eastern",
+  "Native Hawaiian/Pacific Islander",
+  "East Asian",
+  "Southeast Asian",
+  "South Asian",
+  "Central Asian",
+  "White or Caucasian",
+  "Other:",
+];
+
+const genderOptions = ["Female", "Male", "Non-binary", "Other:"];
+
+const minorityCategoriesOptions = [
+  "LGBTQIA2S+",
+  "Immigrant",
+  "BIPOC",
+  "Indigenous person",
+  "Person with a disability (visible/invisible)",
+  "First-Generation college student",
+  "Person from a low-income household",
+];
+
+const firstToPursueTechOptions = ["Yes", "No"];
+
+const timeStudyingTechOptions = ["Less than 1 year", "1-2 years", "3+ years"];
+
+function HackerDemographicForm({
+  data,
+  setData,
+  handleChange,
+  handleSubmit,
+}: HackerDemographicFormProps) {
+  return (
+    <div className="p-24 flex flex-col h-full bg-navPrimary relative">
+      <div className="absolute inset-0 z-7 pointer-events-none">
+        <img
+          src="/hackerformsgraphic.svg"
+          alt="Hacker Forms Graphic Background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute top-0 left-0 w-full h-1/4 pointer-events-none z-5 bg-gradient-to-b from-backgroundSecondary to-navPrimary" />
+      <div className="pb-5 relative z-10">
+        <div className="flex gap-2 items-center pb-10">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-semibold text-textSecondary">
+              Demographic Questions 🗺️
+            </h1>
+            <p>
+              We’d like to understand our applicants’ backgrounds to improve
+              accessibility. These questions are for statistical purposes only.
+            </p>
+            <p>
+              Your information will be kept confidential in accordance with our
+              <a
+                href="https://mlh.io/privacy"
+                className="text-blue-500 underline ml-1"
+              >
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </div>
+          <Image
+            src="/themed_assets/Sunflower.png"
+            alt="grass"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+
+      <form className="form z-10" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-24 text-textPrimary">
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-base">
+              What is your ethnicity? Select all that apply.
+            </span>
+            <MultiCheckbox
+              options={ethnicityOptions}
+              selected={data.ethnicity}
+              onChange={(selected) =>
+                setData((prev: any) => ({ ...prev, ethnicity: selected }))
+              }
+              otherValue={data.ethnicityOther}
+              onOtherChange={(val) =>
+                setData((prev: any) => ({ ...prev, ethnicityOther: val }))
+              }
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-base">
+              What’s your gender identity?
+            </span>
+            <div className="flex flex-col gap-2">
+              {genderOptions.map((option) => (
+                <CheckOff
+                  key={option}
+                  name="gender"
+                  value={option}
+                  label={option}
+                  checked={data.gender === option}
+                  onChange={handleChange}
+                  otherValue={
+                    option === "Other:" ? data.genderOther : undefined
+                  }
+                  onOtherChange={
+                    option === "Other:"
+                      ? (val) =>
+                          setData((prev: any) => ({
+                            ...prev,
+                            genderOther: val,
+                          }))
+                      : undefined
+                  }
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-base">
+              Are any of the following categories relevant to you? Please select
+              all that apply.
+            </span>
+            <MultiCheckbox
+              options={minorityCategoriesOptions}
+              selected={data.minorityCategories}
+              onChange={(selected) =>
+                setData((prev: any) => ({
+                  ...prev,
+                  minorityCategories: selected,
+                }))
+              }
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-base">
+              Are you the first in your immediate family to pursue tech?
+            </span>
+            <div className="flex flex-col gap-2">
+              {firstToPursueTechOptions.map((choice) => (
+                <CheckOff
+                  key={choice}
+                  name="firstToPursueTech"
+                  value={choice}
+                  label={choice}
+                  checked={data.firstToPursueTech === choice}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <span className="font-bold text-base">
+              How long have you been involved in tech?
+            </span>
+            <div className="flex flex-col gap-2">
+              {timeStudyingTechOptions.map((time) => (
+                <CheckOff
+                  key={time}
+                  name="timeStudyingTech"
+                  value={time}
+                  label={time}
+                  checked={data.timeStudyingTech === time}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-end mt-8">
+          <SubmitButton>→</SubmitButton>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default HackerDemographicForm;
