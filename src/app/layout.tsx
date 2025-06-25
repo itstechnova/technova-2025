6a0a6ffce0484b62d33452fc2fdef7e86c91f200
Angela Xu <angela.xu.dev@gmail.com>
2025-06-25 23:54:21 +0900

    set up user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-import { AccountProvider } from "@/components/AccountContext";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Poppins } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+import { AccountProvider } from '@/components/AccountContext';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 const poppinsFont = Poppins({
-  weight: ["500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
+  weight: ['500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
-  title: "TechNova 2025",
-  description: "made with love by the TechNova team",
+  title: 'TechNova 2025',
+  description: 'made with love by the TechNova team',
 };
 
 export default function RootLayout({
@@ -44,27 +44,25 @@ export default function RootLayout({
             target="_blank"
             rel="noopener noreferrer"
             style={{
-              display: "block",
-              maxWidth: "120px",
-              minWidth: "80px",
-              position: "absolute", // change to "absolute" since you want it to scroll with the page
-              right: "70px",
-              top: "0",
-              width: "10%",
+              display: 'block',
+              maxWidth: '120px',
+              minWidth: '80px',
+              position: 'absolute', // change to "absolute" since you want it to scroll with the page
+              right: '70px',
+              top: '0',
+              width: '10%',
               zIndex: 10000,
             }}
           >
             <img
               src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
               alt="Major League Hacking 2026 Hackathon Season"
-              style={{ width: "100%" }}
+              style={{ width: '100%' }}
             />
           </a>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <div className="flex-grow">{children}</div>
-            <Footer />
-          </div>
+          <Navbar />
+          {children}
+          <Footer />
         </AccountProvider>
       </body>
     </html>
