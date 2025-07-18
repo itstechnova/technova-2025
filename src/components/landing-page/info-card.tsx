a5397095baa51cc323d19e395d7060dc26604736
Kim Guo <kbrqin@gmail.com>
2025-07-18 01:27:27 -0400

    add use client to info card

--- src/components/landing-page/info-card.tsx
+++ src/components/landing-page/info-card.tsx
@@ -2,10 +2,6 @@
 
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-"use client";
-
-import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 
 interface InfoCardProps {
   flowerIndex: number;
