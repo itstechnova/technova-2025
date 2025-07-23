dba49d9a2368711bf1fbe1a692f5e5b4a9a8460f
miaisakovic <mia.isak011@gmail.com>
2025-07-23 01:54:51 -0400

    Fixed styling in hacker applications

--- src/components/hacker/landingform.tsx
+++ src/components/hacker/landingform.tsx
@@ -46,14 +46,13 @@ function HackerLandingForm({
   }, []);
 
   return (
-	
     <div className="relative min-h-screen bg-navPrimary">
       <div
-        className="fixed inset-x-0 top-0 h-1/3 pointer-events-none z-0
-                   bg-gradient-to-b from-backgroundTertiary to-navPrimary"
+        className="absolute top-0 left-0 w-full h-1/4 pointer-events-none z-5
+                   bg-gradient-to-b from-backgroundSecondary to-navPrimary"
       />
-      <div className="pt-24 relative z-10 mx-auto px-6 lg:px-24 py-12">
-        <div className="flex items-center gap-2 pb-24">
+      <div className="pt-10 md:pt-24 relative z-10 mx-auto px-6 lg:px-24 py-12">
+        <div className="flex items-center gap-2 pb-10 md:pb-24">
           <h1 className="text-4xl md:text-5xl font-semibold text-textSecondary">
             TechNova 2025 Hacker Application
           </h1>
@@ -62,6 +61,7 @@ function HackerLandingForm({
             alt="grass"
             width={30}
             height={30}
+            className="hidden md:block"
           />
         </div>
         <div className="prose max-w-none prose-lg prose-stone mb-8 prose-headings:font-semibold prose-headings:text-2xl prose-a:text-blue-600 prose-a:underline hover:prose-a:text-blue-800 prose-em:text-gray-700 prose-em:italic">
@@ -72,44 +72,46 @@ function HackerLandingForm({
             {introMd}
           </ReactMarkdown>
         </div>
-      </div>
 
-      <div className="py-32 text-left w-1/2 md:w-3/5 relative z-10">
-        <form onSubmit={handleSubmit} className="form">
-          <div className="flex flex-col gap-24 text-textPrimary">
-            <ShortAnswerQuestion
-              question="Email"
-              name="email"
-              id="email"
-              type="email"
-              placeholder="ex. janesmith@gmail.com"
-              value={data.email}
-              onChange={(e) => {
-                handleChange(e);
-                updateData({ email: e.target.value });
-              }}
-            />
-            <ShortAnswerQuestion
-              question="How old will you be as of September 27, 2025?"
-              name="age2025"
-              id="age2025"
-              placeholder="ex. 21"
-              min={0}
-              max={100}
-              value={data.age2025}
-              onChange={(e) => {
-                handleChange(e);
-                updateData({ age2025: e.target.value });
-              }}
-            />
-          </div>
-          {formError && <p className="text-red-500">{formError}</p>}
-          <SubmitButton>→</SubmitButton>
-        </form>
+        <div className="pb-48 md:pb-5 relative z-10">
+          <form onSubmit={handleSubmit} className="form">
+            <div className="md:w-1/2">
+              <div className="flex flex-col gap-24 text-textPrimary">
+                <ShortAnswerQuestion
+                  question="Email"
+                  name="email"
+                  id="email"
+                  type="email"
+                  placeholder="ex. janesmith@gmail.com"
+                  value={data.email}
+                  onChange={(e) => {
+                    handleChange(e);
+                    updateData({ email: e.target.value });
+                  }}
+                />
+                <ShortAnswerQuestion
+                  question="How old will you be as of September 27, 2025?"
+                  name="age2025"
+                  id="age2025"
+                  placeholder="ex. 21"
+                  min={0}
+                  max={100}
+                  value={data.age2025}
+                  onChange={(e) => {
+                    handleChange(e);
+                    updateData({ age2025: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+            {formError && <p className="text-red-500">{formError}</p>}
+            <SubmitButton>→</SubmitButton>
+          </form>
+        </div>
       </div>
 
       <Image
-        className="absolute bottom-0 right-0 z-10 pointer-events-none"
+        className="absolute bottom-0 right-0 z-5 pointer-events-none"
         src="/themed_assets/bunnywithflower.svg"
         alt="flower bunny"
         width={800}
